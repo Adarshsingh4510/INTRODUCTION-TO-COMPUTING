448b6ea113169fc7cc939df68622cf968582e569
Adarshsingh4510 <96662368+Adarshsingh4510@users.noreply.github.com>
2022-01-07 22:51:28 +0530

    Add files via upload

---
+++ output_file_asg1_21105089_ECE.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:/Users/hp/asg1_21105089_ECE.py ==================
+Solution Number 1
+Enter the Num1- 45
+Enter the Num1- 55
+Enter the Num1- 65
+AVERAGE OF THREE NUMBER ENTERED-  55.0
+Solution Number 2
+Enter the Gross income to the nearest penny-  100000
+Enter the number of Dependents- 4
+15600.0
+Solution Number 3
+Student-[Sid , Name , Gender , Department name , CGPA]
+Student-  [21105089, 'Adarsh', 'M', 'CSE', 9.5]
+Solution Number 4
+Marks before sorting 
+[89, 97, 95, 93, 85]
+Marks after sorting 
+[85, 89, 93, 95, 97]
+Solution Number 5(a)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Solution Number 5(b)
+['Red', 'Green', 'White', 'Purple', 'Yellow']
