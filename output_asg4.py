9dd3507ddea4cfb2b726966bd1f2f0e47035d7b1
ADARSH MANDLOI <96662368+Adarshsingh4510@users.noreply.github.com>
2022-03-01 23:35:38 +0530

    Add files via upload

---
+++ output_asg4.py
@@ -0,0 +1,78 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/hp/asg4_21105089.py ====================
+Question1
+
+problem of tower of Hanoi with three disks.
+
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Question2
+
+Enter the line till which you want to see pascal triangle which must be greater than 0: 5
+With recursion
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+Without recursion using loops to get factorial
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+Question 3
+
+enter first no. :11
+enter secod no. :12
+(0, 11)
+
+Part a)
+True
+
+Part b)
+
+Part c)
+[0, 11, 4, 5, 6]
+11 5 6 
+
+Part d)
+{11, 5, 6}
+
+Part e)
+the immutable set is : frozenset({11, 5, 6})
+
+Part f)
+max value : 11
+Hash value : 11
+
+Question 4
+
+Adarsh Mandloi
+21105089
+Record of  Adarsh Mandloi  destroyed
+
+Question 5
+
+Part a
+Updated salary : 70000
+Part b
+e3 object deleted
+
+Question 6
+
+Give a word to test friendship :care
+Give a meaningful word containing same letter of the word given by your friend :race
+does the word entered by 2nd friend seems meaningfull ? (y/n) :y
+Test passed thus  friendship is true
