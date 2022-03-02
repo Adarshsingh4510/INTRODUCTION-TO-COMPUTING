1a93d2d819908a7624716a064adce871f4572952
ADARSH MANDLOI <96662368+Adarshsingh4510@users.noreply.github.com>
2022-03-02 11:12:49 +0530

    Add files via upload

---
+++ outputfile_asg4_21105089.py
@@ -0,0 +1,84 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/hp/asg4_21105089.py ====================
+Question1
+
+problem of tower of Hanoi with three disks.
+
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+Number of counts : 7
+
+Question2
+
+Enter the line till which you want to see pascal triangle which must be greater than 0: 5
+With recursion
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+With iteration
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+Question 3
+
+enter first no. :234
+enter secod no. :12
+(19, 6)
+
+Part a)
+True
+
+Part b)
+both values are non zero
+
+Part c)
+[19, 6, 4, 5, 6]
+19 6 5 6 
+
+Part d)
+{19, 5, 6}
+
+Part e)
+the immutable set is : frozenset({19, 5, 6})
+
+Part f)
+max value : 19
+Hash value : 19
+
+Question 4
+
+Name of s1: Adarsh Mandloi
+Roll number of s1: 21105089
+Record of  Adarsh Mandloi  destroyed
+Object s1 deleted
+
+Question 5
+
+Mehak is an employee having salary 40000
+Ashok is an employee having salary 50000
+Viren is an employee having salary 60000
+Part a
+Updated salary of Mehak is  : 70000
+Part b
+Deleted the record of Viren with salary 60000
+
+Question 6
+
+Give a word to test friendship :care
+Give a meaningful word containing same letter of the word given by your friend :race
+does the word entered by 2nd friend seems meaningfull ? (y/n) :y
+Test passed thus  friendship is true
